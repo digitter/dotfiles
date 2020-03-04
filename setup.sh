057a4975e7b70f438abcd9e4811316ea6cd855dd
kazuki <matchingserver@gmail.com>
2020-03-05 04:31:01 +0900

    add vscode settings.json

--- setup.sh
+++ setup.sh
@@ -10,9 +10,16 @@
      sales.sh
      …
     )
+for file in ${DOT_FILES[@]}
+do
+    ln -s $HOME/dotfiles/$file $HOME/$file
+done
 
- for file in ${DOT_FILES[@]}
- do
-     ln -s $HOME/dotfiles/$file $HOME/$file
- done
+VSCODE=(
+    settings.json
+)
+for file in ${VSCODE[@]}
+do
+    ln -s $HOME/dotfiles/$file $HOME/Library/Application\ Support/Code/User/$file
+done
 
